f866ce4b41651b8079c840818126ce7191d5c569
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-05-17 20:19:56 -0700

    commit tree a8d1c90d4f331220b3e62566e46a4c30bac3a958

---
+++ index.js
@@ -0,0 +1,24 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function fn (name) {
+    return name
+}
+
+function returnsANamedFunction() {
+    return fn;
+}
+
+
+
+//anonymous function
+
+// function fx (name) {
+//     return ''
+// }
+
+function returnsAnAnonymousFunction(){
+   return 1;
+}
